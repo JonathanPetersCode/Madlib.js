1f7b81e6344d0b9059e14b77c62b99c065cac4a5
Jonathan Peters <42306733+JonathanPetersCode@users.noreply.github.com>
2018-09-03 15:41:16 -0400

    Create JSPractice.js

---
+++ JSPractice.js
@@ -0,0 +1,5 @@
+var noun1 = prompt("Enter a noun:");
+var verb = prompt (" Enter a verb: ");
+var noun2 = prompt(" Enter a second noun: ");
+alert("Your mad lib is complete, here you go!");
+document.write("The ugly " + noun1 + " stood on his head, while " + verb + "ing" + " with the " + noun2 + "!!!");
